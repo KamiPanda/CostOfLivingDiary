0393ea2b84d7cf1ac7863a29ebf30be498680c71
Billy Wu <bb551188@gmail.com>
2013-12-06 13:34:10 -0500

    list of countries

--- src/com/example/costoflivingdiary/AddPreferenceActivity.java
+++ src/com/example/costoflivingdiary/AddPreferenceActivity.java
@@ -10,51 +10,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-<<<<<<< HEAD
+
 
 public class AddPreferenceActivity extends Activity{
 	
-	private Button mSubmit;
-	private Button mCancel;
-	private Spinner mCountriesList;
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.addpref);
-		mCountriesList = (Spinner) findViewById(R.id.countryPicker);
-		List<String> list = new ArrayList<String>();
-		//Todo fill this list with actual stuff
-		list.add("list 1");
-		list.add("list 2");
-		list.add("list 3");
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-			android.R.layout.simple_spinner_item, list);
-		dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-		mCountriesList.setAdapter(dataAdapter);
-		
-		mSubmit = (Button) findViewById(R.id.prefSubmit);
-		mSubmit.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	String country = String.valueOf(mCountriesList.getSelectedItem());
-            	//Todo need to implement preferences
-            	PreferenceItem item = new PreferenceItem(country, false);
-            	MainActivity.PREF_LIST.add(item);
-            	onBackPressed();
-            }
-		});
-		
-		mCancel = (Button) findViewById(R.id.prefCancel);
-		
-		mCancel.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	onBackPressed();
-            }
-		});
-	}
-}
-=======
- 
- public class AddPreferenceActivity extends Activity{
-   
    private Button mSubmit;
    private Button mCancel;
    private Spinner mCountriesList;
@@ -113,4 +72,3 @@ public class AddPreferenceActivity extends Activity{
      });
    }
   }
->>>>>>> 98d00b55cb5d63a18636e4dec87d6f48f9983ecc
